a2c238ab9b853e182776c6c4e15a4a3bd674dcb3
fire156flow <fire156flow@gmail.com>
2020-05-27 15:27:13 +0900

    アセンブリ化以外終了

--- compiler2020/work/parse.c
+++ compiler2020/work/parse.c
@@ -8,34 +8,48 @@ extern FILE *outfile;
 void error(char *s);
 void statement(void);
 
-void compiler(void){
+void compiler(void)
+{
 	init_getsym();
 
 	getsym();
 
-	if (tok.attr == RWORD && tok.value == PROGRAM){
+	if (tok.attr == RWORD && tok.value == PROGRAM)
+	{
 
 		getsym();
 
-		if (tok.attr == IDENTIFIER){
+		if (tok.attr == IDENTIFIER)
+		{
 
 			getsym();
 
-			if (tok.attr == SYMBOL && tok.value == SEMICOLON){
+			if (tok.attr == SYMBOL && tok.value == SEMICOLON)
+			{
 
 				getsym();
 
 				statement();
 
-				if (tok.attr == SYMBOL && tok.value == PERIOD){
+				if (tok.attr == SYMBOL && tok.value == PERIOD)
+				{
 					fprintf(stderr, "Parsing Completed. No errors found.\n");
-				} else error("At the end, a period is required.");
-			}else error("After program name, a semicolon is needed.");
-		}else error("Program identifier is needed.");
-	}else error("At the first, program declaration is required.");
+				}
+				else
+					error("At the end, a period is required.");
+			}
+			else
+				error("After program name, a semicolon is needed.");
+		}
+		else
+			error("Program identifier is needed.");
+	}
+	else
+		error("At the first, program declaration is required.");
 }
 
-void error(char *s){
+void error(char *s)
+{
 	fprintf(stderr, "%s\n", s);
 	exit(1);
 }
@@ -44,7 +58,65 @@ void error(char *s){
 // Parser
 //
 
-void statement(void){
-
+void print_tok()
+{
+	printf("attr=%d\tvalue=%d\tcharvalue=%s\tsline=%d\n", tok.attr, tok.value, tok.charvalue, tok.sline);
 }
 
+void statement(void)
+{
+	print_tok();
+
+	if (tok.attr == NUMBER)
+	{
+		getsym();
+
+		if (tok.attr == SYMBOL || tok.attr == RWORD)
+		{
+			switch (tok.value)
+			{
+			case PLUS:
+				print_tok();
+				getsym();
+				print_tok();
+				break;
+			case MINUS:
+				print_tok();
+				getsym();
+				print_tok();
+				break;
+			case TIMES:
+				print_tok();
+				getsym();
+				print_tok();
+				break;
+			case DIV:
+				print_tok();
+				getsym();
+				print_tok();
+				break;
+			}
+			getsym();
+		}
+	}
+	else if (tok.attr == RWORD)
+	{
+		if (tok.value == BEGIN)
+		{
+			getsym();
+			statement();
+			print_tok();
+		}
+	}
+
+	if (tok.value == SEMICOLON)
+	{
+		getsym();
+		statement();
+	}
+
+	if (tok.value == END)
+	{
+		getsym();
+	}
+}
